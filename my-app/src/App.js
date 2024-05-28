832857828f337c4f97123bb06611b7fb8841846f
Hu Man Mak <e1156942@u.nus.edu>
2024-05-28 17:18:43 +0800

    Revert "login signup button switch"
    This reverts commit fa2adb8ccb3843361ee4e5759181ded088a6d280.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,24 @@
-import "./styles.css";
-import React from "react";
-import { useState } from "react";
-import Signup from "./Components/Signup";
-import Signin from "./Components/Signin";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState("Signup");
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  };
   return (
-    <>
-      <div className="App">
-        {currentForm === "Signup" ? (
-          <Signup toggleForm={toggleForm} />
-        ) : (
-          <Signin toggleForm={toggleForm} />
-        )}
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
