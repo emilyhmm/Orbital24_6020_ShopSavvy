45ab2a651e1f06de29ebb24eef7981838f8d045d
CL <ongcheeling04@gmail.com>
2024-06-11 21:34:19 +0800

    Organised code into MVC architecture

---
+++ Orbital24_6020_ShopSavvy/backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser")
+const connectDB = require("./db")
+const User = require("./models/User")
+const cors = require("cors")
+const bcrypt = require("bcryptjs")
+
+const app = express()
+app.use(cors())
+connectDB();
+
+//body parsers
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/api/user', authRouter);
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('server is running on port',PORT)
+});
+
