7b26a113807d792e064e5daf1a13ecd2f5053e43
CL <ongcheeling04@gmail.com>
2024-06-17 19:09:51 +0800

    Webscrapper can now scrape multiple pages

--- backend/controllers/ProductController.js
+++ backend/controllers/ProductController.js
@@ -7,7 +7,7 @@ const search = asyncHandler(async (req, res) => {
     const term = req.body;
     const searchpage = "https://www.amazon.sg/s?k=".concat(term);
     let result = [];
-    console.log(searchpage);
+    let isNextDisabled = false;
 
     const browser = await puppeteer.launch({
         headless: false,
@@ -18,35 +18,60 @@ const search = asyncHandler(async (req, res) => {
     const page = await browser.newPage();
     await page.goto(searchpage);
 
-    //finds elements on page that match selector
-    const productHandles = await page.$$(
-        "div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item"
-    ); 
+    while(!isNextDisabled) {
+        //finds elements on page that match selector
+        const productHandles = await page.$$(
+            "div.s-main-slot.s-result-list.s-search-results.sg-row > .s-result-item"
+        ); 
 
-    //looping through every element to extract out product title, price and image
-    for (const i of productHandles) {
+        //looping through every element to extract out product title, price and image
+        for (const i of productHandles) {
 
-        let title = "Null";
-        let price = "Null";
-        let image = "Null";
+            let title = "Null";
+            let price = "Null";
+            let image = "Null";
+            let link = "Null"
 
+            try {
+                title = await page.evaluate(el => el.querySelector("h2 > a > span").textContent, i);
+            } catch (error){}
+
+            try {
+                price = await page.evaluate(el => el.querySelector(".a-price > .a-offscreen").textContent, i);
+            } catch (error){}
+
+            try {
+                image = await page.evaluate(el => el.querySelector(".s-image").getAttribute("src"), i);
+            } catch (error){}
+            
+            try {
+                link = await page.evaluate(el => el.querySelector("h2 > a").getAttribute("href"), i);
+                link = `https://www.amazon.sg${link}`; //to construct the full link
+            } catch (error){ 
+                console.log(error);
+            }
+
+            //filtering out null values
+            if(title !== "Null" && price !== "Null") {
+                result.push({"title": title, "price": price, "image": image, "link": link})
+            }
+
+        }
+        
         try {
-            title = await page.evaluate(el => el.querySelector("h2 > a > span").textContent, i);
-        } catch (error){}
+            await page.waitForSelector('.s-pagination-item.s-pagination-next', { visible: true });
+            let next_disabled = await page.$('span.s-pagination-item.s-pagination-next.s-pagination-disabled') !== null;
+            isNextDisabled = next_disabled;
 
-        try {
-            price = await page.evaluate(el => el.querySelector(".a-price > .a-offscreen").textContent, i);
-        } catch (error){}
-
-        try {
-            image = await page.evaluate(el => el.querySelector(".s-image").getAttribute("src"), i);
-        } catch (error){}
-
-        //filtering out null values
-        if(title !== "Null") {
-            result.push({"title": title, "price": price, "image": image})
+            if (!next_disabled) {
+                await Promise.all([
+                    page.click('a.s-pagination-item.s-pagination-next.s-pagination-button.s-pagination-separator'),
+                    page.waitForNavigation({ waitUntil: 'networkidle2' })
+                ]);
+            }
+        } catch (error) {
+            isNextDisabled = true;
         }
     }
-
     console.log(result);
 });
