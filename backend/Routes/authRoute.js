736ef66b974bac0ac57b84c917406a450d3f4ebb
emilyhmm <e1156942@u.nus.edu>
2024-06-18 23:51:57 +0800

    Delete backend/Routes directory - duplicates

--- backend/Routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createUser, loginUser } = require('../Controllers/UserController');
-const router = express.Router();
-
-router.post('/register', createUser);
-router.post('/login', loginUser);
-
-module.exports = router;
