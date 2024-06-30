c2130f891fa3caffa034affe47a7c5a124a17876
emilyhmm <e1156942@u.nus.edu>
2024-06-30 15:36:25 +0800

    merges

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -15,32 +15,33 @@ function Header({ cart }) {
                 <Searchbar/>
             </div>
             <div className = "header__nav">
-            {/*1st link */}
-            <Link to = "/login" className = "header__link">
-                <div className = "header__option">
-                    <span className = "header__optionLineOne"> Hello, </span>
-                    <span className = "header__optionLineTwo">Sign In or Sign Out</span>
-                </div>
-            </Link>
-        
-            {/*2nd link */}
-            <Link to = "/" className = "header__link">
-                <div className = "header__option">
-                    <span className = "header__optionLineOne"> Returns </span>
-                    <span className = "header__optionLineTwo">& Orders</span>
-                </div>
-            </Link>
+              {/*1st link */}
+              <Link to = "/login" className = "header__link">
+                  <div className = "header__option">
+                      <span className = "header__optionLineOne"> Hello, </span>
+                      <span className = "header__optionLineTwo">Sign In or Sign Out</span>
+                  </div>
+              </Link>
+          
+              {/*2nd link */}
+              <Link to = "/" className = "header__link">
+                  <div className = "header__option">
+                      <span className = "header__optionLineOne"> Returns </span>
+                      <span className = "header__optionLineTwo">& Orders</span>
+                  </div>
+              </Link>
 
-            {/*Basket Icon with number*/}
-            <Link to = "/checkout" className = "header__link"> 
-                <div className = "header__optionBasket">
-                    <ShoppingCartIcon/>
-                    {/*Number of items in basket */}
-                    <span className = 'header_optionLineTwo header__basketCount' >{quantity}</span>
-                </div>
-            </Link>
+              {/*Basket Icon with number*/}
+              <Link to = "/checkout" className = "header__link"> 
+                  <div className = "header__optionBasket">
+                      <ShoppingCartIcon/>
+                      {/*Number of items in basket */}
+                      <span className = 'header_optionLineTwo header__basketCount' >{quantity}</span>
+                  </div>
+              </Link>
+            </div>
         </nav>
       )
-    )
+    }
 
 export default Header;
